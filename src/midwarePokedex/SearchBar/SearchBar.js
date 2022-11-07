03bf29a1680e9549c90baa9c50fd2e0052132197
AMVee <66985992+AMVee@users.noreply.github.com>
2022-11-07 08:00:40 -0600

    add more styling to search bar

--- src/midwarePokedex/SearchBar/SearchBar.js
+++ src/midwarePokedex/SearchBar/SearchBar.js
@@ -1,22 +1,44 @@
 import "./SearchBar.css";
+import searchIcon from "./assets/searchIcon.png";
 
 import { useState } from "react";
 
 function SearchBar(props) {
+  const [showBar, setShowBar] = useState(false);
+
   function handleChange(e) {
     props.handleChange(e.target.value);
   }
 
+  function handleIconClick() {
+    setShowBar(!showBar);
+  }
+
   const searchBarValue = props.searchBarValue;
+
+  let SearchBarBoxClassName = "SearchBar-Box";
+  if (!showBar) {
+    SearchBarBoxClassName += " BarInvisible";
+  }
+
+  let SearchBarIconClassName = "SearchBar-Icon";
+  if (!showBar) {
+    SearchBarBoxClassName += " SearchBarIconCollapsed";
+  }
 
   return (
     <div className="SearchBar-container">
+      <img
+        src={searchIcon}
+        className={SearchBarIconClassName}
+        onClick={handleIconClick}
+      />
       <input
         type="text"
         name="name"
-        className="searchbar"
         value={searchBarValue}
         onChange={handleChange}
+        className={SearchBarBoxClassName}
       />
     </div>
   );
